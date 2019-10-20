c621738da6a31f1e79d341e0cd2f29b55eb877bf
SamuraiNights <jonathan.y.wen@icloud.com>
2019-10-20 11:30:59 +0800

    added tutorial requirments.

--- activities/Abecedarium.activity/js/activity.js
+++ activities/Abecedarium.activity/js/activity.js
@@ -1,6 +1,6 @@
 ﻿
 
-define(["sugar-web/activity/activity","sugar-web/datastore"], function (activity, datastore) {
+define(["sugar-web/activity/activity","sugar-web/datastore","tutorial"], function (activity, datastore, tutorial) {
 	if (!Abcd) {
 		Abcd = {};
 	}
@@ -34,6 +34,42 @@ define(["sugar-web/activity/activity","sugar-web/datastore"], function (activity
 		// Stop sound at end of game to sanitize media environment, specifically on Android
 		document.getElementById("stop-button").addEventListener('click', function (event) {
 			Abcd.sound.pause();
+		});
+		var helpButton = document.getElementById("help-button");
+		helpButton.addEventListener('click', function(e) {
+			tutorial.setElement("activity", document.getElementById("activity-button"));
+			tutorial.setElement("stop", document.getElementById("stop-button"));
+			tutorial.setElement("undo", document.getElementById("undo-button"));
+			tutorial.setElement("redo", document.getElementById("redo-button"));
+			tutorial.setElement("node", nodetextButton);
+			tutorial.setElement("link", linkButton);
+			tutorial.setElement("remove", removeButton);
+			tutorial.setElement("png", pngButton);
+			tutorial.setElement("zoom", zoomButton);
+			tutorial.setElement("textvalue", textValue);
+			tutorial.setElement("bold", boldButton);
+			tutorial.setElement("italic", italicButton);
+			tutorial.setElement("foreground", foregroundButton);
+			tutorial.setElement("background", backgroundButton);
+			tutorial.setElement("font", fontButton);
+			tutorial.setElement("fontminus", fontMinusButton);
+			tutorial.setElement("fontplus", fontPlusButton);
+			tutorial.setElement("board", document.getElementById('canvas'));
+			if (cy) {
+				var selected = lastSelected;
+				if (!selected) {
+					var nodes = cy.elements("node");
+					if (nodes.length > 0) {
+						selected = nodes[0];
+						lastSelected = selected;
+						selectNode(selected);
+					}
+				}
+				if (selected) {
+					showSubToolbar(selected);
+				}
+			}
+			tutorial.start();
 		});
 	});
 
